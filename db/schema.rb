6758ebd9c99e48e6db1ffc174bc5bfef1bae9ae0
John Higgins <johnhiggins@outlook.ie>
2019-02-08 18:11:29 +0800

    added methods index, new, edit, show and associated views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_02_08_040036) do
-
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "details"
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2019_02_08_040036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
